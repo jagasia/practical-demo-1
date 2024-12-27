5ca0f77efb41b92c441c27073cac6d883b217ca1
srinumanikanta katha <srinumanikanta0011@gmail.com>
2024-12-27 20:23:20 +0530

    created

--- 27-dec-project-1/src/Employee.java
+++ 27-dec-project-1/src/Employee.java
@@ -1,4 +1,27 @@
 
 public class Employee {
 
+private int id;
+    private String name;
+    private String department;
+
+     
+    public void setAndDisplayEmployeeDetails(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+
+        
+        System.out.println("Employee Details:");
+        System.out.println("ID: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Department: " + this.department);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.setAndDisplayEmployeeDetails(1, "mani", "Engineering");
+    }
+}
+
 }
